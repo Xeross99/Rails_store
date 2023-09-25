a846c0cd4996c96dad0e053d03f62bec936f8e20
Xeross99 <m.krzysteczko@icloud.com>
2023-09-25 12:09:32 +0200

    add to cart animation

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -23,7 +23,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     follow_redirect!
 
     assert_select 'h2', 'Your Pragmatic Cart'
-    assert_select 'li', '1 × Programming Ruby 1.9'
+    assert_select 'li', 'Programming Ruby 1.9'
   end
 
   test "should show line_item" do
@@ -47,5 +47,14 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to line_items_url
+  end
+
+  test "should create line_item via turbo-stream" do
+    assert_difference("LineItem.count") do
+      post line_items_url, params: { product_id: products(:ruby).id },
+      as: :turbo_stream
+    end
+    assert_response :success
+    assert_match /<tr class="line-item-highlight">/, @response.body
   end
 end
