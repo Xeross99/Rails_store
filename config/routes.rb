62e8c3b73ee4ddee18cdcd238d4b38c9569a7422
Xeross99 <m.krzysteczko@icloud.com>
2023-10-17 12:16:52 +0200

    admin

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
     delete 'logout' => :destroy
   end
 
-  get 'sessions/create'
-  get 'sessions/destroy'
   resources :users
   resources :orders
   resources :line_items
