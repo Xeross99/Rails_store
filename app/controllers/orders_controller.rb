b219330be72d5d61ba2377ec6445fbd7a96e351a
Xeross99 <m.krzysteczko@icloud.com>
2023-10-18 15:48:33 +0200

    wip

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,12 @@
 class OrdersController < ApplicationController
   #Przywołanie CurrentCart z app/models/concerns/current_cart.rb
-include CurrentCart
+  include CurrentCart
+  
   before_action :set_cart, only: %i[ new create ]
   before_action :ensure_cart_isnt_empty, only: %i[ new ]
   before_action :set_order, only: %i[ show edit update destroy ]
   skip_before_action :authorize, only: %i[ new create ]
+  
   # GET /orders or /orders.json
   def index
     @orders = Order.all
