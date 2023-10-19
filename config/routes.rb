3fc4dd23bdbc375d33acd1593b852706b1c9a61c
Xeross99 <m.krzysteczko@icloud.com>
2023-10-19 09:39:54 +0200

    Add new language

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,16 @@ Rails.application.routes.draw do
     delete 'logout' => :destroy
   end
 
-  root 'store#index', as: 'store_index'
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
   resources :products do
     get :who_bought, on: :member
   end
+
+  scope '(:locale)' do 
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index'
+  end
+
 end
