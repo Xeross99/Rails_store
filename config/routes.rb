4c7b638f3f63698b44cafa40290f83e02f51739c
Xeross99 <m.krzysteczko@icloud.com>
2023-10-19 07:46:13 +0200

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -3,22 +3,19 @@ Rails.application.routes.draw do
   get 'static_pages/news', as: 'news'
   get 'static_pages/contact', as: 'contact'
   get 'admin' => 'admin#index'
+  
   controller :sessions do
     get 'login' => :new
     post 'login' => :create
     delete 'logout' => :destroy
   end
 
+  root 'store#index', as: 'store_index'
   resources :users
   resources :orders
   resources :line_items
   resources :carts
-  root 'store#index', as: 'store_index'
   resources :products do
     get :who_bought, on: :member
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
