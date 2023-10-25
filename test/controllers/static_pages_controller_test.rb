d50cc0508275dea6c94fc9a5e33744dec7de3c3c
Xeross99 <m.krzysteczko@icloud.com>
2023-10-25 13:54:39 +0200

    fixed tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,17 +2,17 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get questions" do
-    get static_pages_questions_url
+    get questions_url
     assert_response :success
   end
 
   test "should get news" do
-    get static_pages_news_url
+    get news_url
     assert_response :success
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_url
     assert_response :success
   end
 end
